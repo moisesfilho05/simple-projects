92b1fedef71d76015fd234de14a6f29450d61e54
Moisés Filho <filhomoises1708@gmail.com>
2025-02-07 12:53:29 -0300

    initial project: contact book

---
+++ contact-book/app.js
@@ -0,0 +1,28 @@
+const contatos = [];
+
+const adicionarContato = () => {
+    let nome = prompt('Digite o seu nome:');
+    let numero = prompt('Digite o seu número:');
+    const contato = {
+        nome: nome,
+        numero: numero,
+    };
+    contatos.push(contato);
+    console.log(contatos);
+
+    exibirContato();
+};
+
+function exibirContato() {
+    let listaContatos = document.getElementById('listaContatos');
+    listaContatos.innerHTML = '';
+
+    contatos.forEach(contato => {
+        const div = document.createElement('div');
+        div.innerHTML = `<p>nome: ${contato.nome}</p>
+        <p>número: ${contato.numero}</p>
+        <button onclick="">deletar</button>
+        <hr>`
+        listaContatos.appendChild(div);
+    });
+};
